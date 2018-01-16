6c8487fb12727cb3f22d27cb3562255651e10180
Chen <334972898@qq.com>
2018-01-16 16:22:01 +0800

    git 粘包demo tag:2018-1-16-1

--- timeserver/src/main/java/sz/learn/netty/demo/timeserver/utils/TimeClientHandler.java
+++ timeserver/src/main/java/sz/learn/netty/demo/timeserver/utils/TimeClientHandler.java
@@ -11,17 +11,22 @@ public class TimeClientHandler extends ChannelHandlerAdapter {
 
     private static final Logger logger = Logger.getLogger(TimeClientHandler.class.getName());
 
-    private final ByteBuf firstMessage;
+    private int counter;
+
+    private byte[] req;
 
     public TimeClientHandler() {
-        byte[] req = "QUERY TIME ORDER".getBytes();
-        this.firstMessage = Unpooled.buffer(req.length);
-        firstMessage.writeBytes(req);
+        req = ("QUERY TIME ORDER" + System.getProperty("line.separator")).getBytes();
     }
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        ctx.writeAndFlush(firstMessage);
+        ByteBuf message = null;
+        for(int i = 0 ; i < 100 ; ++i) {
+            message = Unpooled.buffer(req.length);
+            message.writeBytes(req);
+            ctx.writeAndFlush(message);
+        }
     }
 
     @Override
@@ -29,9 +34,8 @@ public class TimeClientHandler extends ChannelHandlerAdapter {
         ByteBuf buf = (ByteBuf) msg;
         byte[] req = new byte[buf.readableBytes()];
         buf.readBytes(req);
-
-        String body = new String(req, "UTF-8");
-        System.out.println("Now is : " + body);
+        String body = new String(req, "utf-8");
+        System.out.println("Now is : " + body + " ; the counter is : " + ++counter);
     }
 
     @Override
--- timeserver/src/main/java/sz/learn/netty/demo/timeserver/utils/TimeServerHandler.java
+++ timeserver/src/main/java/sz/learn/netty/demo/timeserver/utils/TimeServerHandler.java
@@ -9,6 +9,8 @@ import java.util.Date;
 
 public class TimeServerHandler extends ChannelHandlerAdapter {
 
+    private int counter;
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
 
@@ -16,19 +18,24 @@ public class TimeServerHandler extends ChannelHandlerAdapter {
 
         byte[] req = new byte[buf.readableBytes()];
         buf.readBytes(req);
-        String body = new String(req, "utf-8");
-        System.out.println("The time server receive order : " + body);
 
-        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date(System.currentTimeMillis()).toString() :
-                "BAD ORDER";
+        String body = new String(req, "utf-8")
+                .substring(0, req.length - System.getProperty("line.separator").length());
 
+        System.out.println("The time server receive order : " + body + " ; the counter is : " + ++counter);
+
+        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date(System.currentTimeMillis()).toString()
+                : "BAD ORDER";
+
+        currentTime = currentTime + System.getProperty("line.separator");
         ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
-        ctx.write(resp);
+        ctx.writeAndFlush(resp);
     }
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-        ctx.flush();
+        //ctx.flush();
+        super.channelReadComplete(ctx);
     }
 
     @Override
